014914d0a5545604611b980d5a9bea0159e05e5a
khashayar norouzi <40909721+khashino@users.noreply.github.com>
2020-05-09 20:17:15 +0430

    Update nohack.py

--- nohack.py
+++ nohack.py
@@ -7,11 +7,11 @@ import argparse
 #https://github.com/danielkrupinski/OneByteWallhack/blob/master/OneByteWallhack.py
 #https://github.com/danielkrupinski/OneByteRadar/blob/master/OneByteRadar.py
 #https://github.com/frk1/hazedumper/blob/master/csgo.hpp
-dwEntityList = (0x4D43AB4)
-dwGlowObjectManager = (0x528B880)
+dwEntityList = (0x4D43AE4)
+dwGlowObjectManager = (0x528B8B8)
 m_iGlowIndex = (0xA428)
 m_iTeamNum = (0xF4)
-dwRadarBase = (0x5178824)
+dwRadarBase = (0x5178854)
 def makeitready():
     global pm
     global client
