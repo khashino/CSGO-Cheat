96ae18eab070277d49573933ab1778567e08b0b6
khashayar norouzi <40909721+khashino@users.noreply.github.com>
2021-03-02 20:43:36 +0330

    Update nohack.py

--- nohack.py
+++ nohack.py
@@ -5,16 +5,17 @@ import time
 import argparse
 
 
-dwEntityList = (0x4D5022C)
-dwGlowObjectManager = (0x5298078)
+dwEntityList = (0x4DA2F34)
+dwGlowObjectManager = (0x52EB550)
 m_iGlowIndex = (0xA438)
 m_iTeamNum = (0xF4)
-dwRadarBase = (0x5184F9C)
+dwRadarBase = (0x51D7CAC)
+dwClientState = (0x58EFE4)
 def makeitready():
     global pm
     global client
     pm = pymem.Pymem("csgo.exe")
-    client = pymem.process.module_from_name(pm.process_handle, "client_panorama.dll")
+    client = pymem.process.module_from_name(pm.process_handle, "client.dll")
 
 
 
@@ -25,50 +26,48 @@ def esp():
 
     while True:
         glow_manager = pm.read_int(client.lpBaseOfDll + dwGlowObjectManager)
-        #print(glow_manager)
+        print(glow_manager)
         for i in range(1, 32):  # Entities 1-32 are reserved for players.
             entity = pm.read_int(client.lpBaseOfDll + dwEntityList + i * 0x10)
-            #print(entity)
+            clientst = pm.read_int(client.lpBaseOfDll + dwClientState + i * 0x10)
+            print (clientst)
             if entity:
                 entity_team_id = pm.read_int(entity + m_iTeamNum)
                 entity_glow = pm.read_int(entity + m_iGlowIndex)
-               # print(entity_team_id)
 
-                if entity_team_id == 2:  # Terrorist
-                    pm.write_float(glow_manager + entity_glow * 0x38 + 0x4, float(1))   # R
-                    pm.write_float(glow_manager + entity_glow * 0x38 + 0x8, float(0))   # G
-                    pm.write_float(glow_manager + entity_glow * 0x38 + 0xC, float(0))   # B
-                    pm.write_float(glow_manager + entity_glow * 0x38 + 0x10, float(0.5))  # Alpha
-                    pm.write_int(glow_manager + entity_glow * 0x38 + 0x24, 1)           # Enable glow
+                print(entity_glow)
 
-                elif entity_team_id == 3:  # Counter-terrorist
-                    pm.write_float(glow_manager + entity_glow * 0x38 + 0x4, float(0))   # R
-                    pm.write_float(glow_manager + entity_glow * 0x38 + 0x8, float(0))   # G
-                    pm.write_float(glow_manager + entity_glow * 0x38 + 0xC, float(1))   # B
-                    pm.write_float(glow_manager + entity_glow * 0x38 + 0x10, float(0.5))  # Alpha
-                    pm.write_int(glow_manager + entity_glow * 0x38 + 0x24, 1)           # Enable glow
 
-                    
+                pm.write_float(glow_manager + entity_glow * 0x38 + 0x4, float(1))   # R
+                pm.write_float(glow_manager + entity_glow * 0x38 + 0x8, float(0))   # G
+                pm.write_float(glow_manager + entity_glow * 0x38 + 0xC, float(0))   # B
+                pm.write_float(glow_manager + entity_glow * 0x38 + 0x10, float(0.5))  # Alpha
+                pm.write_int(glow_manager + entity_glow * 0x38 + 0x24, 1)           # Enable glow
+
+               # Enable glow
+
+
 def wall():
     print("wall secound is on.")
     clientModule = pm.read_bytes(client.lpBaseOfDll, client.SizeOfImage)
     address = client.lpBaseOfDll + re.search(rb'\x83\xF8.\x8B\x45\x08\x0F',clientModule).start() + 2
-    
+
+
     pm.write_uchar(address, 2 if pm.read_uchar(address) == 1 else 1)
-    
+
 def radar():
     print("radar is on.")
     clientModule = pm.read_bytes(client.lpBaseOfDll, client.SizeOfImage)
-    address = client.lpBaseOfDll + re.search(rb'\x80\xB9.{5}\x74\x12\x8B\x41\x08', clientModule).start() + 6    
-    
+    address = client.lpBaseOfDll + re.search(rb'\x80\xB9.{5}\x74\x12\x8B\x41\x08', clientModule).start() + 6
+
     pm.write_uchar(address, 0 if pm.read_uchar(address) != 0 else 2)
-    
+
 
 def Money():
     print("radar is on.")
     clientModule = pm.read_bytes(client.lpBaseOfDll, client.SizeOfImage)
     address = client.lpBaseOfDll + re.search(rb'.\x0C\x5B\x5F\xB8\xFB\xFF\xFF\xFF',clientModule).start()
-    
+
     pm.write_uchar(address, 0xEB if pm.read_uchar(address) == 0x75 else 0x75)
 
 def main():
@@ -93,7 +92,7 @@ def main():
     print("")
     print("USAGE:")
     print("1: Wall Hack (Recomended) \n2: radar hack \n3: show enemy money \n4: Another Wall Hack\n5: Turn Off\n6: Exit")
-    
+
     while True:
         cheat = int(input("Enter Number  :  "))
         if cheat == 1:
